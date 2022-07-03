57e736e6021d2f906a888fa070d688aed793462f
废墟 <r.anerg@gmail.com>
2022-07-04 02:16:05 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM caddy:2-alpine
+
+LABEL org.opencontainers.image.authors="r.anerg@gmail.com"
+
+ARG V2R_VERSION=v4.45.2
+ARG DOMAIN
+ARG EMAIL
+
+
+ENV TZ              Asia/Shanghai
+ENV DOMAIN          ${DOMAIN}
+ENV EMAIL           ${EMAIL}
+ENV V2R_URL         https://github.com/v2fly/v2ray-core/releases/download/${V2R_VERSION}/v2ray-linux-64.zip
+ENV V2R_PATH_CONF   /etc/v2ray
+ENV CADDY_PATH_CONF /etc/caddy
+
+ADD boot.sh /usr/bin
+
+COPY conf/ /conf/
+
+RUN set -xe \
+    && apk -U upgrade \
+    && apk add --update --no-cache --virtual .build-deps \
+    tzdata \
+    curl \
+    && mkdir -p \
+    ${CADDY_PATH_CONF} \
+    ${V2R_PATH_CONF} \
+    && cp /usr/share/zoneinfo/${TZ} /etc/localtime \
+    && curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray.zip ${V2R_URL} \
+    && unzip /tmp/v2ray.zip -d /tmp/ \
+    && mv /tmp/v2ray /usr/bin/ \
+    && mv /tmp/v2ctl /usr/bin/ \
+    && chmod +x /usr/bin/v2ray \
+    && chmod +x /usr/bin/v2ctl \
+    && chmod +x /usr/bin/boot.sh \
+    # 删除不必要的东西
+    && apk del .build-deps \
+    && rm -rf /tmp/* \
+    && rm /etc/caddy/Caddyfile \
+    && apk add uuidgen openrc
+
+EXPOSE 443
+
+CMD ["/usr/bin/boot.sh"]
---
+++ boot.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+
+cd $DIR
+
+if [ -z $DOMAIN ];then
+    echo "未设置环境变量-DOMAIN"
+    exit 2
+fi
+
+if [ -z $EMAIL ];then
+    echo "未设置环境变量-EMAIL"
+    exit 2
+fi
+
+if [ ! -e "/etc/caddy/Caddyfile" ];then
+    WS_PATH="`echo $RANDOM | md5sum | cut -c 3-11`"
+    WS_PORT=$(($RANDOM+1234))
+    UUID=$(uuidgen)
+
+    cp -f /conf/caddy/default_Caddyfile /etc/caddy/Caddyfile
+    sed -i "s/example.domain/${DOMAIN}/" /etc/caddy/Caddyfile
+    sed -i "s/ws_path/${WS_PATH}/" /etc/caddy/Caddyfile
+    sed -i "s/1234/${WS_PORT}/" /etc/caddy/Caddyfile
+    sed -i "s/your@email.com/${EMAIL}/" /etc/caddy/Caddyfile
+
+    cp -f /conf/v2ray/default_config.json /etc/v2ray/config.json
+    sed -i "s/1234/${WS_PORT}/" /etc/v2ray/config.json
+    sed -i "s/uuid/${UUID}/" /etc/v2ray/config.json
+    sed -i "s/ws_path/${WS_PATH}/" /etc/v2ray/config.json
+
+    echo "====================================="
+    echo "V2ray 配置信息"
+    echo "地址（address）: ${DOMAIN}"
+    echo "端口（port）： 443"
+    echo "用户id（UUID）： ${UUID}"
+    echo "加密方式（security）： 自适应"
+    echo "传输协议（network）： ws"
+    echo "伪装类型（type）： none"
+    echo "路径（不要落下/）： /${WS_PATH}/"
+    echo "底层传输安全： tls"
+    echo "====================================="
+
+    cp -f /conf/v2ray/default_client.json /etc/v2ray/client.json
+    sed -i "s/hostname-placeholder/${DOMAIN}/" /etc/v2ray/client.json
+    sed -i "s/address-placeholder/${DOMAIN}/" /etc/v2ray/client.json
+    sed -i "s/uuid-placeholder/${UUID}/" /etc/v2ray/client.json
+    sed -i "s/ws_path-placeholder/${WS_PATH}/" /etc/v2ray/client.json
+
+    clientBase64="`cat /etc/v2ray/client.json | base64 -w 0`"
+    echo "vmess://"$clientBase64
+fi
+
+start-stop-daemon --start \
+	-b -1 /var/log/v2ray.log -2 /var/log/v2ray.log \
+	-m -p /run/v2ray.pid \
+	--exec /usr/bin/v2ray -- -config /etc/v2ray/config.json
+
+/usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
